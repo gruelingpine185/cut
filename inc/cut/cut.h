d0cc471684a4ea92af898ad59c1e4ac1a8fd9f2c
gruelingpine185 <66073333+gruelingpine185@users.noreply.github.com>
2024-01-05 20:59:42 -0500

    added options for errors

--- inc/cut/cut.h
+++ inc/cut/cut.h
@@ -2,6 +2,12 @@
 #define CUT_CUT_H
 
 
+typedef enum {
+    CUT_DEFAULT             = 0 << 0,
+    CUT_EXIT_ON_ERROR       = 1 << 0,
+    CUT_ERROR_WITH_REPORT   = 1 << 1
+} cut_option;
+
 typedef void (*cut_setup_fn)(void);
 typedef void (*cut_teardown_fn)(void);
 typedef void (*cut_test_fn)(void);
